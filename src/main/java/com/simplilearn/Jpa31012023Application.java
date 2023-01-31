8cf6b63ce758dbbf43e6702ddd8f15d44797ad16
Dhruvik <dhruvik@simplilearn.com>
2023-01-31 22:00:13 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/Jpa31012023Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa31012023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa31012023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Jpa31012023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa31012023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
